7606ad9e2bbbb7b9981c022db735f877fab9c6f1
Safaraliyev Elshan <elshansafaraliyev@gmail.com>
2019-01-03 22:37:55 +0400

    end of seasons

---
+++ pics/src/components/SearchBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+    state = { term: '' };
+
+    handleFormSubmit = event =>  {
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
+    };
+
+    render() {
+        return (
+            <div className="ui segment">
+                <form className="ui form" onSubmit={this.handleFormSubmit}>
+                    <div className="field">
+                        <label htmlFor="search">Image Search</label>
+                        <input
+                            type="text"
+                            name="search"
+                            id="search"
+                            placeholder="Search"
+                            value={this.state.term}
+                            onChange={e => this.setState({ term: e.target.value })}
+                        />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchBar;
