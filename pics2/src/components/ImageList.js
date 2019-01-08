68921bc7ebc1ef780bd4d52988ae0046a2318a27
elshan <elshansafaraliyev@gmail.com>
2019-01-08 17:56:54 +0400

    grid -9.1

--- pics2/src/components/ImageList.js
+++ pics2/src/components/ImageList.js
@@ -4,8 +4,10 @@ import React from 'react';
 const ImageList = props => {
 
     const images = props.images.map(({ id, urls, description }) => {
-        return <img key={id} src={urls.regular}  alt={description} />;
-    })
+        return (
+            <img key={id} src={urls.regular}  alt={description} />
+        );
+    });
 
     return (
         <div className="image-list">{images}</div>
