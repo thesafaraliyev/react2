d97921d0e357ab1ad4ed982d38bc5edbfc38cb28
elshan <elshansafaraliyev@gmail.com>
2019-01-14 17:52:23 +0400

    11-16

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,28 +1,35 @@
-import React from 'react';
-import SearchBar from './SearchBar';
-import youtube from  '../api/youtube';
-import VideoList from './VideoList';
+import React from 'react'
+import SearchBar from './SearchBar'
+import youtube from '../api/youtube'
+import VideoList from './VideoList'
 
-class App extends React.Component  {
-    state = { videos: [] };
+class App extends React.Component {
+    state = {videos: [], selectedVideo: null}
 
     onTermSubmit = async term => {
         const video = await youtube.get('/search/', {
             params: {
                 q: term
             }
-        });
-        this.setState({videos: video.data.items});
-    };
+        })
+        this.setState({videos: video.data.items})
+    }
 
-    render() {
+    handleVideoSelect = video => {
+        console.log(video)
+    }
+
+    render () {
         return (
-            <div className="ui container">
-                <SearchBar onFormSubmit={this.onTermSubmit} />
-                <VideoList videos={this.state.videos} />
-            </div>
-        );
+          <div className="ui container">
+              <SearchBar onFormSubmit={this.onTermSubmit}/>
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.handleVideoSelect}
+              />
+          </div>
+        )
     }
 }
 
-export default App;
+export default App
