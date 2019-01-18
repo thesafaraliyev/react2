4a38c62bcba763b03ad70b768c71351f71edff0f
elshan <elshansafaraliyev@gmail.com>
2019-01-18 11:32:46 +0400

    songs

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class SongList extends Component {
+    renderList () {
+        return this.props.songs.map((song) => {
+            return (
+              <div className="item" key={song.title}>
+                <div className="right floated content">
+                    <button className="ui button warning">
+                        Select
+                    </button>
+                </div>
+                  <div className="content">{song.title}</div>
+              </div>
+            );
+        })
+    }
+
     render() {
+        console.log(this.props)
         return (
-          <h4>
-              song list
-          </h4>
+          <div className="ui divided list">
+              {this.renderList()}
+          </div>
         );
     }
 }
 
-export default SongList;
+const mapStateToProps = props => {
+    console.log(props.songs)
+    return {songs: props.songs}
+}
+
+export default connect(mapStateToProps)(SongList);
